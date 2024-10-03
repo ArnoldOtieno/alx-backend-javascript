141e077d282321c3c87ed284610867c840be4a73
ArnoldOtieno <arnoldotieno31@gmail.com>
2024-10-03 02:49:08 +0000

    load balancer

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,4 +1,4 @@
-/*load balancer*/
+/*eslint-disable*/
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise.race([chinaDownload, USDownload])
     .then((value) => value);
